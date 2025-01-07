c25dec3817a875329361194f98625a090af95a4c
Shiro <615149633@qq.com>
2025-01-06 18:03:27 -0600

    feat: add code action plugin: actions-preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,7 +46,31 @@ return {
     },
   },
   {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      "mfussenegger/nvim-dap-python", --optional
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+    lazy = false,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+      require("venv-selector").setup()
+    end,
+    keys = {
+      { ",v", "<cmd>VenvSelect<cr>" },
+    },
+  },
+  {
+    "aznhe21/actions-preview.nvim",
+    config = function()
+      require "configs.actions_preview"
+    end,
+  },
+  {
     "j-hui/fidget.nvim",
+    lazy = false,
     config = function()
       require "configs.fidget"
     end,
