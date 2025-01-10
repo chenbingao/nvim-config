6e3de5c8f837ffe609fc40f72790fe7673bfbccf
Shiro <615149633@qq.com>
2025-01-10 16:01:54 +0800

    feat: add close left｜right｜other buffers hotkey

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,28 +6,50 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<leader>fs", "<cmd> w <cr>", { desc = "file save" })
+
 map("n", "K", function()
   vim.lsp.buf.hover()
 end, { desc = "hover" })
+
 map("n", "gn", function()
   vim.diagnostic.goto_next()
 end, { desc = "diagnostic jump next" })
+
 map("n", "gp", function()
   vim.diagnostic.goto_prev()
 end, { desc = "diagnostic jump prev" })
+
 map("n", "gd", "<cmd>Trouble lsp_definitions<CR>", { desc = "lsp definitions (Trouble)" })
 map("n", "gt", "<cmd>Trouble lsp_type_definitions<CR>", { desc = "lsp type definitions (Trouble)" })
 map("n", "gi", "<cmd>Trouble lsp_implementations<CR>", { desc = "lsp implementations (Trouble)" })
 map("n", "gr", "<cmd>Trouble lsp_references<CR>", { desc = "lsp references (Trouble)" })
+
 map("n", "<leader>lc", function()
   require("actions-preview").code_actions()
 end, { desc = "code action" })
+
 map("n", "<leader>lr", function()
   vim.lsp.buf.rename()
 end, { desc = "rename" })
+
+map("n", ",h", function()
+  require("nvchad.tabufline").closeBufs_at_direction "left"
+end, { desc = "close left buffers" })
+
+map("n", ",l", function()
+  require("nvchad.tabufline").closeBufs_at_direction "right"
+end, { desc = "close right buffers" })
+
+map("n", ",j", function()
+  local buf = require "nvchad.tabufline"
+  buf.closeBufs_at_direction "left"
+  buf.closeBufs_at_direction "right"
+end, { desc = "close other buffers" })
+
 map("n", ",k", function()
   require("nvchad.tabufline").close_buffer()
 end, { desc = "buffer close" })
+
 map("n", ",n", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", { desc = "Buffer Diagnostics (Trouble)" })
 map("n", "<leader>ds", "<cmd>Trouble diagnostics toggle focus=true<CR>", { desc = "Diagnostics (Trouble)" })
 
