2d0621c44a00e654d979185bcdf1101356f25e38
Shiro <615149633@qq.com>
2025-01-21 14:01:37 +0800

    feat: add lsp ts config

--- lua/configs/nvim-lspconfig.lua
+++ lua/configs/nvim-lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "pyright", "buf_ls" }
+local servers = { "html", "cssls", "clangd", "pyright", "buf_ls", "ts_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 local custom_overrides = {
