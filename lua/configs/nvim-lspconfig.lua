31548e0bb667fa43fd4d630d0c279542c20e8714
Shiro <615149633@qq.com>
2025-01-21 15:53:24 +0800

    feat: add json lsp config

--- lua/configs/nvim-lspconfig.lua
+++ lua/configs/nvim-lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "pyright", "buf_ls", "ts_ls" }
+local servers = { "html", "cssls", "clangd", "pyright", "buf_ls", "ts_ls", "jsonls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 local custom_overrides = {
