8402ad53d14a42d20abc051756fe7bd110bef68f
Shiro <615149633@qq.com>
2025-03-12 10:37:08 +0800

    feat: update nvim-cmp config

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -34,8 +34,8 @@ cmp.setup {
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.confirm { select = true }
-      elseif require("luasnip").expand_or_jumpable() then
-        require("luasnip").expand_or_jump()
+      -- elseif require("luasnip").expand_or_jumpable() then
+      --   require("luasnip").expand_or_jump()
       else
         fallback()
       end
@@ -44,8 +44,8 @@ cmp.setup {
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.confirm { select = true }
-      elseif require("luasnip").jumpable(-1) then
-        require("luasnip").jump(-1)
+      -- elseif require("luasnip").jumpable(-1) then
+      --   require("luasnip").jump(-1)
       else
         fallback()
       end
