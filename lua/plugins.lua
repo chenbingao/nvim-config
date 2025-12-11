af1e041d9d75e72f2ba360ad58a02996c47e2372
Shiro <615149633@qq.com>
2025-12-11 09:49:13 +0800

    feat: add markview.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -223,4 +223,11 @@ return {
     end,
   },
   { "EdenEast/nightfox.nvim" },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    -- Completion for `blink.cmp`
+    -- dependencies = { "saghen/blink.cmp" },
+  },
 }
