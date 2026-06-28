c255fe13a10af162afd74cb1b420c1bfa67480d3
Shiro <chenba@excellemed.co>
2026-06-28 22:36:13 +0800

    refactor(lsp): 移除硬编码 codelldb 路径，改用 Mason 自动检测

--- lua/lsp/rust.lua
+++ lua/lsp/rust.lua
@@ -1,20 +1,5 @@
 local common = require "lsp.common"
 
--- Update this path
-local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/"
-local codelldb_path = extension_path .. "adapter/codelldb"
-local liblldb_path = extension_path .. "lldb/lib/liblldb"
-local this_os = vim.uv.os_uname().sysname
-
-if this_os:find "Windows" then
-  codelldb_path = extension_path .. "adapter\\codelldb.exe"
-  liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-else
-  liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-end
-
-local cfg = require "rustaceanvim.config"
-
 vim.g.rustaceanvim = {
   server = {
     on_attach = function(client, bufnr)
@@ -33,7 +18,5 @@ vim.g.rustaceanvim = {
     end,
     capabilities = common.capabilities,
   },
-  dap = {
-    adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-  },
+  dap = {},
 }
